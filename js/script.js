6ac81579d8acdd0a5f8872618a07c12e71a83e84
Abner Gomes <abnerbraz2020@gmail.com>
2025-02-21 10:53:24 -0300

    Blog

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const formMateria = document.getElementById('form-materia');
 function carregarMaterias() {
     fetch('data/materias.csv')
         .then(response => response.text())
-        .then(csv =>{
-            const linhas =csv.split('\n');
+        .then(csv => {
+            const linhas = csv.split('\n');
             const materias = [];
-            for (let i = 1; i <linhas,length; i++) { // Ignora a primeira linha (cabeçalho)
+            for (let i = 1; i < linhas, length; i++) { // Ignora a primeira linha (cabeçalho)
                 const valores = linhas[i].split(',');
-                if (valores,leigth === 2) {
-                    materias.push({ titulo: valores[0], conteudo: valores[1]});
+                if (valores, leigth === 2) {
+                    materias.push({ titulo: valores[0], conteudo: valores[1] });
                 }
             }
             exibirMaterias(materias);
@@ -24,7 +24,7 @@ function exibirMaterias(materias) {
     materias.forEach(materia => {
         const divMateria = document.createElement('div');
         divMateria.classList.add('materia');
-        divMateria.innerHTML= `
+        divMateria.innerHTML = `
             <h3>${materia.titulo}</h3>
             <p>${materia.conteudo}</p>
         `;
@@ -45,4 +45,11 @@ function salvarMateria(materia) {
 formMateria.addEventListener('submit', (event) => {
     event.preventDefault();
     const titulo = document.getElementById('titulo').value;
-})
+    const conteudo = document.getElementById('conteudo').value;
+    const novaMateria = { titulo, conteudo };
+    salvarMateria(novaMateria);
+    formMateria.reset();
+});
+
+// Carrega as matérias ao carregar a página
+carregarMaterias();
